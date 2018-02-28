cb69adc73b4b9e9f18e7b8ace3e3ef015ea80425
Colin Zarzycki <zarzycki@ucar.edu>
2018-02-28 11:35:53 -0700

    deleted outdated code tests and old, less efficient digi filter (now replaced by lowmem option by default)

--- code-tests/sleep.sh
+++
@@ -1,9 +0,0 @@
-echo "Hello!"
-
-sleep 10
-
-echo "I just fell asleep!"
-
-sleep 20
-
-ssh balloflight@s483.sureserver.com 'rm /home/balloflight/www/weather/current/*.png ; rm /home/balloflight/www/weather/current/*.txt'
