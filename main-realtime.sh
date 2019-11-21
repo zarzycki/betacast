3b46ae1fd6a580128c295b1b0cf7f95ba1b526a2
Colin Zarzycki <zarzycki@ucar.edu>
2019-11-21 13:19:42 -0800

    test ssh key

--- main-realtime.sh
+++ main-realtime.sh
@@ -19,7 +19,7 @@
 # download dataset, map to CAM grid, and run a forecast.
 #
 # Generally can be executed on login nodes or in the background, ex:
-# $> nohup ./main-realtime.sh &
+# $> nohup ./main-realtime.sh nl.ne30 &
 # but see above for example of PBS options to submit to batch nodes
 #
 # Details can be found in:
