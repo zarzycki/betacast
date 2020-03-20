d8b0022878359d47dadee8bf7ed1602810e9460a
Colin Zarzycki <zarzycki@ucar.edu>
2020-03-20 06:54:13 -0700

    small fix to allow unbound adjustment topo for backwards portability

--- betacast.sh
+++ betacast.sh
@@ -440,7 +440,7 @@ if [ $debug -ne 1 ] ; then
         YYYYMMDDHH=${yearstr}${monthstr}${daystr}${cyclestr} \
        'data_filename = "'$gfs_files_path'/gfs_atm_'$yearstr$monthstr$daystr$cyclestr'.grib2"'  \
        'wgt_filename="'${gfs2seWeights}'"' \
-       'model_topo_file="'${adjust_topo}'"' \
+       'model_topo_file="'${adjust_topo-}'"' \
        'se_inic = "'${sePreFilterIC}'"' )
   elif [ $atmDataType -eq 2 ] ; then
     echo "CD ing to ERA-interim interpolation directory"
@@ -459,7 +459,7 @@ if [ $debug -ne 1 ] ; then
       YYYYMMDDHH=${yearstr}${monthstr}${daystr}${cyclestr} \
      'data_filename = "'$gfs_files_path'/cfsr_atm_'$yearstr$monthstr$daystr$cyclestr'.grib2"'  \
      'wgt_filename="'${gfs2seWeights}'"' \
-     'model_topo_file="'${adjust_topo}'"' \
+     'model_topo_file="'${adjust_topo-}'"' \
      'se_inic = "'${sePreFilterIC}'"' )
   else
     echo "Incorrect model IC entered"
