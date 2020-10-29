a9459963ba6df56fa7e779e4e543d7bf1364018f
Colin Zarzycki <zarzycki@ucar.edu>
2020-10-29 10:30:45 -0700

    add rof patch options for restart file prints

--- patches/patch-gen.sh
+++ patches/patch-gen.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-  CESMROOT=/global/homes/c/czarzyck/E3SM
-  filename="lnd_comp_mct"
-  SUBPATH="components/clm/src/cpl/"
-  CESMCOMPONENT="clm"
-  SOURCEMODDIR=~/betacast/patches/
-  mkdir -p ${SOURCEMODDIR}
-  diff -b -U 3 ${CESMROOT}/${SUBPATH}/${filename}.F90 /global/homes/c/czarzyck/F-betacast-FC5AV1C/SourceMods/src.${CESMCOMPONENT}/${filename}.F90 > ${SOURCEMODDIR}/${filename}.patch
+CESMROOT=/global/homes/c/czarzyck/E3SM-dev/
+filename="rof_comp_mct"
+SUBPATH="components/mosart/src/cpl/"
+CESMCOMPONENT="mosart"
+SOURCEMODDIR=~/betacast/patches/
+mkdir -p ${SOURCEMODDIR}
+diff -b -U 3 ${CESMROOT}/${SUBPATH}/${filename}.F90 /global/homes/c/czarzyck/v1.2-CONUS-F2010C5-v3/SourceMods/src.${CESMCOMPONENT}/${filename}.F90 > ${SOURCEMODDIR}/${filename}.patch
