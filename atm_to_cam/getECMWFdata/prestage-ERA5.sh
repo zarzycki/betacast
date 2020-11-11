a408cb4fd7bbb82daa8aa931241fef05e0f434da
Colin Zarzycki <zarzycki@ucar.edu>
2020-11-11 16:23:27 -0700

    ERA5 prestage improvements

--- atm_to_cam/getECMWFdata/prestage-ERA5.sh
+++ atm_to_cam/getECMWFdata/prestage-ERA5.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # python3 must be loaded with cdsapi package
-# usage: ./prestage-ERA5.sh 1996011800
+# on cheyenne...
+# $> module load python
+# $> ncar_pylib
+#
+# usage: ./prestage-ERA5.sh /glade/work/${LOGNAME}/sewx/ECMWF/ 1996011800
 # where you want the data file for Jan 18, 1996 at 00Z
 
-DATE=$1
+DATE=$2
+DIR=$1
+THISDIR=$PWD
 
 yearstr=${DATE:0:4}
 monthstr=${DATE:4:2}
@@ -11,7 +17,9 @@ daystr=${DATE:6:2}
 cyclestr=${DATE:8:2}
 
 set -e
-python getERA5.py ${yearstr}${monthstr}${daystr} ${cyclestr}
+mkdir -p $DIR
+cd $DIR
+python $THISDIR/getERA5.py ${yearstr}${monthstr}${daystr} ${cyclestr}
 ncrename -v z,phis ERA5_sfc_${yearstr}${monthstr}${daystr}${cyclestr}.nc
 ncks -A ERA5_ml_${yearstr}${monthstr}${daystr}${cyclestr}.nc ERA5_sfc_${yearstr}${monthstr}${daystr}${cyclestr}.nc
 mv -v ERA5_sfc_${yearstr}${monthstr}${daystr}${cyclestr}.nc ERA5_${yearstr}${monthstr}${daystr}${cyclestr}.nc
