ba21cb20816fa340a4f1956edda35d2daff5332c
Colin Zarzycki <zarzycki@ucar.edu>
2022-10-14 17:23:01 -0700

    updates to e3sm batch clone

--- batch-clone.sh
+++ batch-clone.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-FARRAY=( "RoS-F2010C5-ne0conus30x8-101-plus1K" \
-  "RoS-F2010C5-ne0conus30x8-101-plus2K" \
-  "RoS-F2010C5-ne0conus30x8-101-plus3K" \
-  "RoS-F2010C5-ne0conus30x8-101-plus4K" \
-  "RoS-F2010C5-ne0conus30x8-101-PI" )
+FARRAY=( "RoS-F2010CICEROF-ne0wus30x32-101-plus1K" )
+  
+IARRAY=( "RoS-ICLM45-ne0wus30x32-201_19961225_0060_2019" )
 
-IARRAY=( "RoS-ICLM45-ne0conus30x8-101_19961225_0036_2019" \
-  "RoS-ICLM45-ne0conus30x8-101_19961225_0036_2044" \
-  "RoS-ICLM45-ne0conus30x8-101_19961225_0036_2064" \
-  "RoS-ICLM45-ne0conus30x8-101_19961225_0036_2081" \
-  "RoS-ICLM45-ne0conus30x8-101_19961225_0036_1921" )
+# FARRAY=( "RoS-F2010CICEROF-ne0wus30x32-101-plus1K" \
+#   "RoS-F2010CICEROF-ne0wus30x32-101-plus2K" \
+#   "RoS-F2010CICEROF-ne0wus30x32-101-plus3K" \
+#   "RoS-F2010CICEROF-ne0wus30x32-101-plus4K" \
+#   "RoS-F2010CICEROF-ne0wus30x32-101-PI" )
+#   
+# IARRAY=( "RoS-ICLM45-ne0wus30x32-201_19961225_0060_2019" \
+#   "RoS-ICLM45-ne0wus30x32-201_19961225_0060_2044" \
+#   "RoS-ICLM45-ne0wus30x32-201_19961225_0060_2064" \
+#   "RoS-ICLM45-ne0wus30x32-201_19961225_0060_2081" \
+#   "RoS-ICLM45-ne0wus30x32-201_19961225_0060_1921" )
 
+# FARRAY=( "RoS-F2010C5-ne0conus30x8-101-plus1K" \
+#   "RoS-F2010C5-ne0conus30x8-101-plus2K" \
+#   "RoS-F2010C5-ne0conus30x8-101-plus3K" \
+#   "RoS-F2010C5-ne0conus30x8-101-plus4K" \
+#   "RoS-F2010C5-ne0conus30x8-101-PI" )
+# 
+# IARRAY=( "RoS-ICLM45-ne0conus30x8-101_19961225_0036_2019" \
+#   "RoS-ICLM45-ne0conus30x8-101_19961225_0036_2044" \
+#   "RoS-ICLM45-ne0conus30x8-101_19961225_0036_2064" \
+#   "RoS-ICLM45-ne0conus30x8-101_19961225_0036_2081" \
+#   "RoS-ICLM45-ne0conus30x8-101_19961225_0036_1921" )
+  
 # FARRAY=( "RoS-F2010C5-ne0conus30x8-006-control" \
 #   "RoS-F2010C5-ne0conus30x8-006-plus1K" \
 #   "RoS-F2010C5-ne0conus30x8-006-plus2K" \
@@ -26,11 +42,12 @@ IARRAY=( "RoS-ICLM45-ne0conus30x8-101_19961225_0036_2019" \
 #   "RoS-ICLM45-ne0conus30x8-006_19960113_0012_2081" \
 #   "RoS-ICLM45-ne0conus30x8-006_19960113_0012_1921" )
 
-MASTERCASENAME=RoS-F2010C5-ne0conus30x8-101-control
+#MASTERCASENAME=RoS-F2010C5-ne0conus30x8-101-control
+MASTERCASENAME=RoS-F2010CICEROF-ne0wus30x32-101-control
+E3SMPATH=/global/homes/c/czarzyck/E3SM-20220616/
+
 BETACAST=/global/homes/c/czarzyck/betacast/
 PATHTOCASE=~/F-compsets
-E3SMPATH=~/E3SM-dev/
-GENERICDATES=dates.WUS.starting.txt
 
 for i in $(seq 0 $(( ${#FARRAY[@]} - 1 )))
 do
@@ -42,7 +59,7 @@ do
   ./create_clone --case ${PATHTOCASE}/${CASENAME} --clone ${PATHTOCASE}/${MASTERCASENAME} --keepexe
   cd ${PATHTOCASE}/${CASENAME}
   ./case.setup --silent
-  #./case.build --silent
+  ./case.build --silent
 
   echo "now doing land stuff"
   
@@ -50,12 +67,13 @@ do
   cd ~/scratch/e3sm_scratch/cori-knl/${CASENAME}/run
   mkdir landstart
   cd landstart
+  cp -v ~/scratch/e3sm_scratch/cori-knl/${LANDSPINUPDIR}/run/*.elm.r.*.nc /global/homes/c/czarzyck/m2637/betacast/elm-inic/1997wus
+  cp -v ~/scratch/e3sm_scratch/cori-knl/${LANDSPINUPDIR}/run/*.mosart.r.*.nc /global/homes/c/czarzyck/m2637/betacast/elm-inic/1997wus
   cp -v ~/scratch/e3sm_scratch/cori-knl/${LANDSPINUPDIR}/run/*.elm.r.*.nc .
   cp -v ~/scratch/e3sm_scratch/cori-knl/${LANDSPINUPDIR}/run/*.mosart.r.*.nc .
   rename -v ${LANDSPINUPDIR} ${CASENAME} *nc
 
   cd ${BETACAST}
-  cp -v ${GENERICDATES} dates.${CASENAME}.txt
   
   echo "****** DONE!"
 done
