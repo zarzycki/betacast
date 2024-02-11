4e24b84b65c1c4930221f10e61043b8640e18303
Colin Zarzycki <zarzycki@ucar.edu>
2024-02-11 14:36:43 -0800

    add first commit for model to datm

---
+++ land-spinup/datm-from-model/batch-model-to-datm.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+################################################################
+#### PMCPU
+################################################################
+#SBATCH --qos=regular
+#SBATCH --time=11:30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --constraint=cpu
+################################################################
+
+dryrun=false
+H5DIR=/pscratch/sd/c/czarzyck/hyperion/CHEY.VR28.NATL.REF.CAM5.4CLM5.0.dtime900/h5/
+SCRIPTDIR=$PWD
+
+SERVER_NAME=$(hostname -A)
+echo $SERVER_NAME
+
+if [[ $SERVER_NAME == *"perlmutter"* ]] || [[ $SERVER_NAME == *"nid0"* ]]; then
+  echo "Using pm-cpu"
+  export NCARG_ROOT=/global/homes/c/czarzyck/.conda/pkgs/ncl-6.6.2-h3fdc804_41/
+  PATHTONCL=/global/homes/c/czarzyck/.conda/envs/e3sm_unified_1.8.1_nompi/bin/
+  source /global/common/software/e3sm/anaconda_envs/load_latest_e3sm_unified_pm-cpu.sh
+  module load parallel
+  NUMCORES=42
+elif [[ $SERVER_NAME == *"casper"* ]] || [[ $SERVER_NAME == *"crhtc"* ]]; then
+  echo "Using Casper"
+  module load parallel
+  module load peak_memusage
+  NUMCORES=36
+else
+  echo "Unrecognized server. exiting"
+  exit
+fi
+
+# GNUPARALLEL SETTINGS
+TIMESTAMP=`date +%s%N`
+COMMANDFILE=commands.${TIMESTAMP}.txt
+
+FILES=`find $H5DIR -type f -name "*.nc"`
+
+for f in $FILES; do
+  COMMAND="bash $SCRIPTDIR/single-file-to-datm.sh $f"
+  echo ${COMMAND} >> ${COMMANDFILE}
+done
+
+if [ $dryrun = false ] ; then
+  echo "Launching GNU parallel"
+  # Launch GNU parallel
+  if [[ $SERVER_NAME == *"perlmutter"* ]] || [[ $SERVER_NAME == *"nid0"* ]]; then
+    parallel --jobs ${NUMCORES} < ${COMMANDFILE}
+  elif [[ $SERVER_NAME == *"casper"* ]] || [[ $SERVER_NAME == *"crhtc"* ]]; then
+    peak_memusage.exe parallel --jobs ${NUMCORES} --workdir $PWD < ${COMMANDFILE}
+  else
+    echo "unknown" ; exit
+  fi
+
+  # Cleanup
+  rm ${COMMANDFILE}
+fi
+
---
+++ land-spinup/datm-from-model/single-file-to-datm.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+MAPFILE=~/m2637/betacast/regrid_maps/map_ne0np4natlanticref.ne30x4_TO_era5_0.25x0.25_patc.nc
+
+#SOURCE=/pscratch/sd/c/czarzyck/hyperion/CHEY.VR28.NATL.REF.CAM5.4CLM5.0.dtime900/h5/CHEY.VR28.NATL.REF.CAM5.4CLM5.0.dtime900.cam.h5.1989-11-30-00000.nc
+SOURCE=$1
+
+OUTDIRBASE="/pscratch/sd/c/czarzyck/hyperion/DATM/"
+
+SOURCEDIR="${SOURCE%/*}/"
+SOURCEFILE="${SOURCE##*/}"
+SOURCEDATE="${SOURCEFILE##*.h?.}"
+SOURCEDATE="${SOURCEDATE%.nc}"
+SOURCECASE="${SOURCEFILE%%.?am.h*}"
+
+echo "SOURCEDIR: $SOURCEDIR"
+echo "SOURCEFILE: $SOURCEFILE"
+echo "SOURCEDATE: $SOURCEDATE"
+echo "SOURCECASE: $SOURCECASE"
+
+OUTDIR=${OUTDIRBASE}/${SOURCECASE}
+
+mkdir -pv $OUTDIR
+mkdir -pv $OUTDIR/Solr/
+mkdir -pv $OUTDIR/Precip/
+mkdir -pv $OUTDIR/TPHWL/
+
+TMPsolar=$OUTDIR/Solr/${SOURCEDATE}_solar.nc
+TMPprecip=$OUTDIR/Precip/${SOURCEDATE}_precip.nc
+TMPstate=$OUTDIR/TPHWL/${SOURCEDATE}_state.nc
+
+FINALsolar=$OUTDIR/Solr/${SOURCECASE}_${SOURCEDATE}_Solr.nc
+FINALprecip=$OUTDIR/Precip/${SOURCECASE}_${SOURCEDATE}_Prec.nc
+FINALstate=$OUTDIR/TPHWL/${SOURCECASE}_${SOURCEDATE}_TPHWL.nc
+
+if [[ -f "$FINALsolar" && -f "$FINALprecip" && -f "$FINALstate" ]]; then
+  echo "All files for $SOURCEDATE already exist. Exiting script."
+  exit 1
+fi
+
+echo "Remapping $SOURCEDATE"
+ncremap -v FSDS -i ${SOURCE} -o ${TMPsolar} -m ${MAPFILE}
+ncremap -v PRECT -i ${SOURCE} -o ${TMPprecip} -m ${MAPFILE}
+ncremap -v FLDS,UBOT,VBOT,QBOT,TBOT,PS -i ${SOURCE} -o ${TMPstate} -m ${MAPFILE}
+
+echo "Inverting latitude dimension because of annoying ERA5 N->S"
+ncpdq -O -a '-lat' ${TMPsolar} ${TMPsolar}
+ncpdq -O -a '-lat' ${TMPprecip} ${TMPprecip}
+ncpdq -O -a '-lat' ${TMPstate} ${TMPstate}
+
+echo "Creating ZBOT and WIND"
+ncap2 -O -s 'ZBOT=UBOT*float(0.0)+float(50.0)' -s 'WIND=sqrt(pow(UBOT,2)+pow(VBOT,2))' ${TMPstate} ${TMPstate}
+
+echo "Updating attributes"
+ncatted -O -a long_name,ZBOT,o,c,"Lowest model level height" ${TMPstate}
+ncatted -O -a units,ZBOT,o,c,"m" ${TMPstate}
+ncatted -O -a long_name,WIND,o,c,"Lowest model level wind speed" ${TMPstate}
+
+echo "Updating units"
+ncap2 -O -s 'PRECT=PRECT*float(1000.0)' ${TMPprecip} ${TMPprecip}
+ncatted -O -a units,PRECT,o,c,"mm/s" ${TMPprecip}
+
+echo "Renaming variables to match ERA5 streams"
+ncrename -v PS,PSRF ${TMPstate}
+
+echo "Deleting variables"
+ncks -O -x -v area,gw ${TMPsolar} ${TMPsolar}
+ncks -O -x -v UBOT,VBOT,area,gw ${TMPstate} ${TMPstate}
+ncks -O -x -v area,gw ${TMPprecip} ${TMPprecip}
+
+echo "Converting to nc3"
+nccopy -k nc6 ${TMPsolar} ${FINALsolar}
+nccopy -k nc6 ${TMPprecip} ${FINALprecip}
+nccopy -k nc6 ${TMPstate} ${FINALstate}
+
+echo "Deleting bad files"
+rm -v ${TMPstate} ${TMPsolar} ${TMPprecip}
+
+echo "all done"
