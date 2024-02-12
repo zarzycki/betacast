562238c69a3bf82760612bba803058eb1bead190
Colin Zarzycki <zarzycki@ucar.edu>
2024-02-12 12:13:23 -0800

    large update to auto-script in land spinup

--- land-spinup/auto-script.sh
+++ land-spinup/auto-script.sh
@@ -13,13 +13,15 @@ source ../utils.sh
 #./auto-script.sh 0 0 20200103 36 1 -1 -1 NAMELIST.MACHINE
 # E3SM
 #./auto-script.sh 1 0 19960113 12 1 2018 1920 NAMELIST.MACHINE
+# Model
+#./auto-script.sh 1 2 19840101 0 1 -1 -1 nl.landspinup.pm-cpu
 
 if [[ $# -ne 8 ]] ; then echo "Need 8 inputs, got $#, exiting..." ; exit ; fi
 if [ ! -s $8 ]; then echo "Namelist is empty, exiting..." ; exit ; fi
 
 ### User settings
 modelSystem=${1}         # 0 = CESM/E3SMv1, 1 = E3SMv2
-doERA5=${2}              # Use ERA5 DATM? 0 = yes, 1 = no (internal CRUNCEP)
+doERA5=${2}              # Use ERA5 DATM? 0 = yes, 1 = no (internal CRUNCEP), 2 CESM/E3SM
 FORECASTDATE=${3}        # What is the date you want to spin up for (00Z)
 NMONTHSSPIN=${4}         # Duration of spinup (somewhere b/w 3-12 months seems reasonable)
 NCYCLES=${5}
@@ -36,17 +38,24 @@ fi
 
 # Read the namelist
 read_bash_nl "${NAMELISTFILE}"
-# Set user things to empty strings if not provided
+
+# Set user things to empty strings or internal defaults if not provided
 if [ -z "${USER_FSURDAT+x}" ]; then USER_FSURDAT=""; fi
 if [ -z "${USER_FINIDAT+x}" ]; then USER_FINIDAT=""; fi
 if [ -z "${USER_ICOMPSET+x}" ]; then USER_ICOMPSET=""; fi
 if [ -z "${USER_JOB_PRIORITY+x}" ]; then USER_JOB_PRIORITY=""; fi
+if [ -z "${BUILD_ONLY+x}" ]; then BUILD_ONLY=false; fi
+
+# Set directories to empty strings for use DATM
+if [ -z "${BETACAST_DATM_FORCING_BASE+x}" ]; then BETACAST_DATM_FORCING_BASE=""; fi
+if [ -z "${BETACAST_DATM_ANOMALY_BASE+x}" ]; then BETACAST_DATM_ANOMALY_BASE=""; fi
+
+# Check bools
+check_bool "BUILD_ONLY" $BUILD_ONLY
 
 # Derived settings that should be same between all machines
 BETACAST_DATMDOMAIN=${BETACAST}/land-spinup/gen_datm/gen-datm/
 BETACAST_ANOMALIGN=1920
-BETACAST_STREAMBASE=${BETACAST_DATM_FORCING_BASE}/ERA5/
-BETACAST_ANOMBASE=${BETACAST_DATM_FORCING_BASE}/ANOM_LENS/
 
 if [ $modelSystem -eq 0 ]; then
   echo "Using CESM"
@@ -64,7 +73,6 @@ if [[ -n "$USER_ICOMPSET" ]]; then
   echo "*** OVERRIDING ICOMPSET WITH USER_ICOMPSET!"
   COMPSET=$USER_ICOMPSET
 fi
-
 
 ### Do not edit below this line!
 ### Date logic
@@ -92,6 +100,10 @@ if [ $doERA5 -eq 0 ]; then
   echo "Using ERA5 DATM"
   ERA5STYR=1990
   ERA5ENYR=2022
+elif [ $doERA5 -eq 2 ]; then
+  echo "Using Hyperion DATM"
+  ERA5STYR=1984
+  ERA5ENYR=2014
 else
   echo "Using CRUNCEP DATM"
 fi
@@ -124,7 +136,6 @@ if [ $addDeltas -eq 0 ]; then
   ICASENAME=${ICASENAME}_${BETACAST_ANOMYEAR}
 fi
 
-
 ### Put a block to check everything here?
 echo "--------------------------------------------"
 echo "modelSystem: "${modelSystem}
@@ -136,8 +147,8 @@ echo "addDeltas: "${addDeltas}
 echo "BETACAST_ANOMYEAR: "${BETACAST_ANOMYEAR}
 echo "BETACAST_ANOMALIGN: "${BETACAST_ANOMALIGN}
 echo "BETACAST_DATMDOMAIN: "${BETACAST_DATMDOMAIN}
-echo "BETACAST_STREAMBASE: "${BETACAST_STREAMBASE}
-echo "BETACAST_ANOMBASE: "${BETACAST_ANOMBASE}
+echo "BETACAST_DATM_FORCING_BASE: "${BETACAST_DATM_FORCING_BASE}
+echo "BETACAST_DATM_ANOMALY_BASE: "${BETACAST_DATM_ANOMALY_BASE}
 echo "CIMEROOT: "${CIMEROOT}
 echo "PATHTOCASE: "${PATHTOCASE}
 echo "ICASENAME: "${ICASENAME}
@@ -148,6 +159,7 @@ echo "RESOL: "${RESOL}
 echo "RUNQUEUE: "${RUNQUEUE}
 echo "USER_JOB_PRIORITY: "${USER_JOB_PRIORITY}
 echo "WALLCLOCK: "${WALLCLOCK}
+echo "BUILD_ONLY: "${BUILD_ONLY}
 echo "ICASENAME: "${ICASENAME}
 echo "USER_FSURDAT: "${USER_FSURDAT}
 echo "USER_FINIDAT: "${USER_FINIDAT}
@@ -169,50 +181,93 @@ cd ${PATHTOCASE}/${ICASENAME}
 ./xmlchange DATM_CLMNCEP_YR_END=${FORECASTYEAR}
 ./xmlchange DATM_CLMNCEP_YR_ALIGN=${FORECASTYEARM1}
 ./xmlchange RUN_STARTDATE=${STARTDATE}
-./xmlchange STOP_DATE=${FORECASTDATE}
 ./xmlchange REST_OPTION='end'
 ./xmlchange DOUT_S=FALSE
+# If NMONTHSSPIN is 0, doesn't make sense to stop model on same day
+if [ $NMONTHSSPIN -gt 0 ]; then
+  ./xmlchange STOP_DATE=${FORECASTDATE}
+fi
 
 ### If using ERA5, add the stream files and reset DATM_CLMNCEP_YR_START, etc.
 if [ $doERA5 -eq 0 ]; then
   echo "Injecting ERA5 DATM streams"
   cp ${BETACAST}/land-spinup/streams/user_datm.streams.txt.CLMCRUNCEPv7* .
   #REPLACEDIR
-  sed -i "s?\${BETACAST_STREAMBASE}?${BETACAST_STREAMBASE}?g" user_datm.streams.txt.CLMCRUNCEPv7.Solar
+  sed -i "s?\${BETACAST_STREAMBASE}?${BETACAST_DATM_FORCING_BASE}?g" user_datm.streams.txt.CLMCRUNCEPv7.Solar
   sed -i "s?\${BETACAST_DATMDOMAIN}?${BETACAST_DATMDOMAIN}?g" user_datm.streams.txt.CLMCRUNCEPv7.Solar
-  sed -i "s?\${BETACAST_STREAMBASE}?${BETACAST_STREAMBASE}?g" user_datm.streams.txt.CLMCRUNCEPv7.Precip
+  sed -i "s?\${BETACAST_STREAMBASE}?${BETACAST_DATM_FORCING_BASE}?g" user_datm.streams.txt.CLMCRUNCEPv7.Precip
   sed -i "s?\${BETACAST_DATMDOMAIN}?${BETACAST_DATMDOMAIN}?g" user_datm.streams.txt.CLMCRUNCEPv7.Precip
-  sed -i "s?\${BETACAST_STREAMBASE}?${BETACAST_STREAMBASE}?g" user_datm.streams.txt.CLMCRUNCEPv7.TPQW
+  sed -i "s?\${BETACAST_STREAMBASE}?${BETACAST_DATM_FORCING_BASE}?g" user_datm.streams.txt.CLMCRUNCEPv7.TPQW
   sed -i "s?\${BETACAST_DATMDOMAIN}?${BETACAST_DATMDOMAIN}?g" user_datm.streams.txt.CLMCRUNCEPv7.TPQW
   ./xmlchange DATM_CLMNCEP_YR_ALIGN=${ERA5STYR}
+elif [ $doERA5 -eq 2 ]; then
 
-  if [ $NCYCLES -lt 2 ]; then
-    ### NO CYCLE
-    ./xmlchange DATM_CLMNCEP_YR_START=${ERA5STYR}
-    ./xmlchange DATM_CLMNCEP_YR_END=${ERA5ENYR}
-    # Update general vars in case needed for anom stream overwrite
-    FORECASTYEARM1=${ERA5STYR}
-    FORECASTYEAR=${ERA5ENYR}
-  fi
+  VARS=("Precip" "Solar" "TPQW")
+  for VAR in "${VARS[@]}"; do
+
+    NEW_STREAM=./user_datm.streams.txt.CLMCRUNCEPv7.${VAR}
+    DATMVARFOLDER="${BETACAST_DATM_FORCING_BASE}/${VAR}/"
+
+    cp -v ${BETACAST}/land-spinup/streams/GENERIC/user_datm.streams.txt.CLMCRUNCEPv7.${VAR} ${NEW_STREAM}
+
+    TMPFILELIST=${VAR}_filelist.txt
+
+    ls -1 "$DATMVARFOLDER" > ${TMPFILELIST}
+    # Temporary files for before and after the placeholder
+    BEFORE_TEMP=$(mktemp)
+    AFTER_TEMP=$(mktemp)
+
+    # Split the XML file at "FILES_HERE"
+    awk "/FILES_HERE/{exit} 1" "$NEW_STREAM" > "$BEFORE_TEMP"
+    awk "x==1{print} /FILES_HERE/{x=1}" "$NEW_STREAM" > "$AFTER_TEMP"
+
+    # Concatenate: before the placeholder, file list, and after the placeholder
+    cat "$BEFORE_TEMP" > "$NEW_STREAM"
+    cat ${TMPFILELIST} >> "$NEW_STREAM"
+    cat "$AFTER_TEMP" >> "$NEW_STREAM"
+
+    # Remove any trailing newlines at the end of the file
+    sed -i '/^$/d' "$NEW_STREAM"
+
+    # Clean up temporary files
+    rm -v "$BEFORE_TEMP" "$AFTER_TEMP" "$TMPFILELIST"
+
+    #### Other stuff
+    sed -i "s?\${BETACAST_STREAMBASE}?${DATMVARFOLDER}?g" "$NEW_STREAM"
+    sed -i "s?\${BETACAST_DATMDOMAIN}?${BETACAST_DATMDOMAIN}?g" "$NEW_STREAM"
+  done
+
+  ./xmlchange DATM_CLMNCEP_YR_ALIGN=${ERA5STYR}
+
 fi
+
+if { [ $doERA5 -eq 0 ] || [ $doERA5 -eq 2 ]; } && [ $NCYCLES -lt 2 ]; then
+  ### NO CYCLE
+  ./xmlchange DATM_CLMNCEP_YR_START=${ERA5STYR}
+  ./xmlchange DATM_CLMNCEP_YR_END=${ERA5ENYR}
+  # Update general vars in case needed for anom stream overwrite
+  FORECASTYEARM1=${ERA5STYR}
+  FORECASTYEAR=${ERA5ENYR}
+fi
+
 
 if [ $addDeltas -eq 0 ]; then
   echo "Injecting anomaly DATM streams"
   cp ${BETACAST}/land-spinup/streams/user_datm.streams.txt.Anomaly.* .
   #REPLACEDIR
-  sed -i "s?\${BETACAST_ANOMBASE}?${BETACAST_ANOMBASE}?g" user_datm.streams.txt.Anomaly.Forcing.Humidity
+  sed -i "s?\${BETACAST_DATM_ANOMALY_BASE}?${BETACAST_DATM_ANOMALY_BASE}?g" user_datm.streams.txt.Anomaly.Forcing.Humidity
   sed -i "s?\${BETACAST_DATMDOMAIN}?${BETACAST_DATMDOMAIN}?g" user_datm.streams.txt.Anomaly.Forcing.Humidity
-  sed -i "s?\${BETACAST_ANOMBASE}?${BETACAST_ANOMBASE}?g" user_datm.streams.txt.Anomaly.Forcing.Temperature
+  sed -i "s?\${BETACAST_DATM_ANOMALY_BASE}?${BETACAST_DATM_ANOMALY_BASE}?g" user_datm.streams.txt.Anomaly.Forcing.Temperature
   sed -i "s?\${BETACAST_DATMDOMAIN}?${BETACAST_DATMDOMAIN}?g" user_datm.streams.txt.Anomaly.Forcing.Temperature
-  sed -i "s?\${BETACAST_ANOMBASE}?${BETACAST_ANOMBASE}?g" user_datm.streams.txt.Anomaly.Forcing.Longwave
+  sed -i "s?\${BETACAST_DATM_ANOMALY_BASE}?${BETACAST_DATM_ANOMALY_BASE}?g" user_datm.streams.txt.Anomaly.Forcing.Longwave
   sed -i "s?\${BETACAST_DATMDOMAIN}?${BETACAST_DATMDOMAIN}?g" user_datm.streams.txt.Anomaly.Forcing.Longwave
-  sed -i "s?\${BETACAST_ANOMBASE}?${BETACAST_ANOMBASE}?g" user_datm.streams.txt.Anomaly.Forcing.Precip
+  sed -i "s?\${BETACAST_DATM_ANOMALY_BASE}?${BETACAST_DATM_ANOMALY_BASE}?g" user_datm.streams.txt.Anomaly.Forcing.Precip
   sed -i "s?\${BETACAST_DATMDOMAIN}?${BETACAST_DATMDOMAIN}?g" user_datm.streams.txt.Anomaly.Forcing.Precip
 
   if [ $BETACAST_REFYEAR -gt 0 ]; then
     # run ncl to normalize things
     echo "Running with normalized deltas"
-    ncl ${BETACAST}/land-spinup/normalize-datm-deltas.ncl 'current_year='${BETACAST_ANOMYEAR}'' 'basedir="'${BETACAST_ANOMBASE}'"'
+    ncl ${BETACAST}/land-spinup/normalize-datm-deltas.ncl 'current_year='${BETACAST_ANOMYEAR}'' 'basedir="'${BETACAST_DATM_ANOMALY_BASE}'"'
     sed -i "s?ens_QBOT_anom.nc?ens_QBOT_${BETACAST_ANOMYEAR}ref_anom.nc?g"   user_datm.streams.txt.Anomaly.Forcing.Humidity
     sed -i "s?ens_TBOT_anom.nc?ens_TBOT_${BETACAST_ANOMYEAR}ref_anom.nc?g"   user_datm.streams.txt.Anomaly.Forcing.Temperature
     sed -i "s?ens_PRECT_anom.nc?ens_PRECT_${BETACAST_ANOMYEAR}ref_anom.nc?g" user_datm.streams.txt.Anomaly.Forcing.Longwave
@@ -239,10 +294,10 @@ fi
 cat > user_nl_elm <<EOF
 do_transient_pfts = .false.
 check_finidat_fsurdat_consistency = .false.
-hist_avgflag_pertape='A','A'
-hist_nhtfrq = 0,-24
-hist_mfilt = 1,10
-hist_fincl2 = 'TSA','TBOT','RAIN','SNOW'
+hist_avgflag_pertape='A','I'
+hist_nhtfrq = 0,-1
+hist_mfilt = 1,24
+hist_fincl2 = 'TSA','TBOT','RAIN','SNOW','QBOT','WIND','SWdown','LWdown'
 EOF
 
 ### USER! Edit this block if using CLM and need to inject any CLM specific mods (e.g., fsurdat, etc.)
@@ -299,6 +354,8 @@ if [[ -n "$USER_JOB_PRIORITY" ]]; then
   echo "Setting job priority based on user preference!"
   ./xmlchange --force JOB_PRIORITY=${USER_JOB_PRIORITY}
 fi
-./case.submit
+if [ "$BUILD_ONLY" = false ]; then
+  ./case.submit
+fi
 
 exit 0
