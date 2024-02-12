dd2b2a80ba2eccb474decccc5bdad6daadb1bfd9
Colin Zarzycki <zarzycki@ucar.edu>
2024-02-12 13:49:13 -0700

    update batch-auto to include some old code

--- land-spinup/batch-auto.sh
+++ land-spinup/batch-auto.sh
@@ -10,3 +10,16 @@ NMONTHS=60
 ./auto-script.sh 1 0 ${YYYYMMDD} ${NMONTHS} 1 2044
 ./auto-script.sh 1 0 ${YYYYMMDD} ${NMONTHS} 1 2064
 ./auto-script.sh 1 0 ${YYYYMMDD} ${NMONTHS} 1 2081
+
+# MPAS stuff for 3km TCs
+#./auto-script.sh 0 1 20050818 12 1 -1 nl.landspinup.mpas
+#./auto-script.sh 0 1 19911122 12 1 -1 nl.landspinup.mpas
+#./auto-script.sh 0 1 19890822 12 1 -1 nl.landspinup.mpas
+#./auto-script.sh 0 1 20080831 12 1 -1 nl.landspinup.mpas
+#./auto-script.sh 0 1 20000918 12 1 -1 nl.landspinup.mpas
+#./auto-script.sh 0 1 20110806 12 1 -1 nl.landspinup.mpas
+#./auto-script.sh 0 1 19950806 12 1 -1 nl.landspinup.mpas
+#./auto-script.sh 0 1 20141013 12 1 -1 nl.landspinup.mpas
+#./auto-script.sh 0 1 20130910 12 1 -1 nl.landspinup.mpas
+#./auto-script.sh 0 1 20120825 12 1 -1 -1 nl.landspinup.mpas
+#./auto-script.sh 0 1 20080818 12 1 -1 -1 nl.landspinup.mpas
