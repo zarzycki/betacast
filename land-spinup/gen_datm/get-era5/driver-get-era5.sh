896d729a17e0f232a501e7ed761b402b5dfd90f4
Colin Zarzycki <zarzycki@ucar.edu>
2024-04-17 14:18:01 -0600

    update ERA5 gen forcing to use noleap calendar

--- land-spinup/gen_datm/get-era5/driver-get-era5.sh
+++ land-spinup/gen_datm/get-era5/driver-get-era5.sh
@@ -7,9 +7,9 @@
 ## declare an array variable
 declare -a months=("01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12")
 
-STYR=2021
-ENYR=2022
-OUTDIR=/glade/scratch/zarzycki/ERA5-DATM/
+STYR=1992
+ENYR=1992
+OUTDIR=/glade/derecho/scratch/zarzycki/ERA5-DATM/
 #OUTDIR="/global/homes/c/czarzyck/scratch/ERA5-DATM/"
 
 mkdir -p -v $OUTDIR
