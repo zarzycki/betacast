245147ef5196520c17ea616967960ebcdfa19857
Colin Zarzycki <zarzycki@ucar.edu>
2025-02-08 12:52:59 -0700

    update sub-crate in atm_to_cam

--- py_atm_to_cam/sub-create.sh
+++ py_atm_to_cam/sub-create.sh
@@ -3,7 +3,7 @@
 ################################################################
 #PBS -N betacast
 #PBS -A P93300042
-#PBS -l select=1:ncpus=1:mem=240GB
+#PBS -l select=1:ncpus=16:mem=240GB
 #PBS -l walltime=23:59:00
 #PBS -q casper@casper-pbs
 #PBS -j oe
@@ -52,7 +52,7 @@ peak_memusage python atm_to_cam.py \
   --write_floats \
   --adjust_config '""' \
   --model_topo_file "/lustre/desc1/scratch/bdobbins/ic_io_err_3.75km/fmthist_3.75km/fmthist_3.75km.init.nc" \
-  --se_inic "/glade/derecho/scratch/zarzycki/betacast_oob_fhist_mpasa3.75km_c250110_v1.nc" \
+  --se_inic "/glade/derecho/scratch/zarzycki/betacast_oob_fhist_mpasa3.75km_c250110_v2.nc" \
   --verbose
 
 date
