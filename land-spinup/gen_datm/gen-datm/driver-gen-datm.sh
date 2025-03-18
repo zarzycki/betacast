4daeec4c20bb0af27cf8f7f20870014145564c4c
Colin Zarzycki <zarzycki@ucar.edu>
2025-03-17 19:53:59 -0600

    move driver to python for land datm gen

--- land-spinup/gen_datm/gen-datm/driver-gen-datm.sh
+++ land-spinup/gen_datm/gen-datm/driver-gen-datm.sh
@@ -4,7 +4,7 @@
 #### Casper
 ################################################################
 #PBS -N forcing-gen
-#PBS -A P93300642
+#PBS -A UPSU0032
 #PBS -l select=1:ncpus=4:mpiprocs=4:mem=80GB
 #PBS -l walltime=24:00:00
 #PBS -q casper@casper-pbs
@@ -12,15 +12,17 @@
 ################################################################
 
 module load parallel
-module load ncl
+#module load ncl
+module load conda
+conda activate npl
 
 echo "parallel location: $(which parallel)"
-echo "ncl location: $(which ncl)"
+#echo "ncl location: $(which ncl)"
 
 NUMCORES=4
 
-STYR=2003
-ENYR=2023
+STYR=2023
+ENYR=2024
 PATHTORAWERA5=/glade/campaign/cgd/amp/zarzycki/DATM_FORCING/raw-ERA5/
 OUTDIRBASE=/glade/campaign/cgd/amp/zarzycki/DATM_FORCING/ERA5/
 
@@ -37,7 +39,8 @@ do
   do
     THISFILE=${PATHTORAWERA5}/out.${DATA_YEAR}.${DATA_MONTH}.nc
     if [ -f "$THISFILE" ]; then
-      LINECOMMAND="ncl gen-forcing.ncl YYYY=$DATA_YEAR 'MM=\"$DATA_MONTH\"' 'RAWERA5FILE=\"$THISFILE\"' 'outdirbase=\"$OUTDIRBASE\"' "
+      #LINECOMMAND="ncl gen-forcing.ncl YYYY=$DATA_YEAR 'MM=\"$DATA_MONTH\"' 'RAWERA5FILE=\"$THISFILE\"' 'outdirbase=\"$OUTDIRBASE\"' "
+      LINECOMMAND="python gen-forcing.py --year=$DATA_YEAR --month=$DATA_MONTH --era5_file=\"$THISFILE\" --outdirbase=\"$OUTDIRBASE\" --do_q --do_flds --greg_to_noleap --convert_nc3"
       echo ${LINECOMMAND} >> ${COMMANDFILE}
     else
       echo "$THISFILE does not exist!"
