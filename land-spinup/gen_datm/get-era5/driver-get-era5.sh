3188c996864ce94a195d11bad2a44b9f36302de5
Colin Zarzycki <zarzycki@ucar.edu>
2025-03-20 05:58:28 -0600

    ERA5 changed how data is served, updates to datm get script

--- land-spinup/gen_datm/get-era5/driver-get-era5.sh
+++ land-spinup/gen_datm/get-era5/driver-get-era5.sh
@@ -5,10 +5,11 @@
 # conda activate meteo
 
 ## declare an array variable
-declare -a months=("01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12")
+#declare -a months=("01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12")
+declare -a months=("01" "02")
 
-STYR=1980
-ENYR=1987
+STYR=2025
+ENYR=2025
 OUTDIR=/glade/campaign/cgd/amp/zarzycki/DATM_FORCING/raw-ERA5/
 #OUTDIR=/glade/derecho/scratch/zarzycki/ERA5-DATM/
 #OUTDIR="/global/homes/c/czarzyck/scratch/ERA5-DATM/"
@@ -22,12 +23,21 @@ do
   do
     echo $jj' '$ii
     OUTFILE=${OUTDIR}/out.${jj}.${ii}.nc
+    OUTZIP=${OUTDIR}/out.${jj}.${ii}.zip
     if [ -f "$OUTFILE" ]; then
       echo "$OUTFILE exists, not downloading"
     else
       echo "$OUTFILE doesn't exist..."
       python get-data.py "${jj}" "${ii}" "${OUTDIR}"
       if command -v ncks >/dev/null 2>&1; then
+        pushd "${OUTDIR}"
+        unzip out.${jj}.${ii}.zip
+        ncks -A data_stream-oper_stepType-instant.nc data_stream-oper_stepType-accum.nc
+        ncks -A data_stream-oper_stepType-accum.nc data_stream-oper_stepType-avg.nc
+        mv -v data_stream-oper_stepType-avg.nc out.${jj}.${ii}.nc
+        rm -v data_stream*.nc
+        rm -v out.${jj}.${ii}.zip
+        popd
         echo "Compressing file using ncks..."
         ncks -O -4 -L 1 "$OUTFILE" "$OUTFILE"
       else
--- land-spinup/gen_datm/get-era5/get-data.py
+++ land-spinup/gen_datm/get-era5/get-data.py
@@ -12,53 +12,51 @@ OUTYEAR=sys.argv[1]
 OUTMONTH=sys.argv[2]
 OUTDIR=sys.argv[3]
 
-OUTVAR = OUTDIR + '/out.' + OUTYEAR + '.'+ OUTMONTH + '.nc'
+#OUTVAR = OUTDIR + '/out.' + OUTYEAR + '.'+ OUTMONTH + '.nc'
+OUTVAR = OUTDIR + '/out.' + OUTYEAR + '.'+ OUTMONTH + '.zip'
 
-c.retrieve(
-    'reanalysis-era5-single-levels',
-    {
-        'product_type': 'reanalysis',
-        'format': 'netcdf',
-        'variable': [
-            '10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature',
-            '2m_temperature', 'mean_total_precipitation_rate', 'surface_pressure',
-            'surface_solar_radiation_downwards', 'surface_thermal_radiation_downwards',
-#            'surface_solar_radiation_downwards',
-        ],
-        'year': OUTYEAR,
-        'month': OUTMONTH,
-        'day': [
-            '01', '02', '03',
-            '04', '05', '06',
-            '07', '08', '09',
-            '10', '11', '12',
-            '13', '14', '15',
-            '16', '17', '18',
-            '19', '20', '21',
-            '22', '23', '24',
-            '25', '26', '27',
-            '28', '29', '30',
-            '31',
-        ],
-#         'time': [
-#             '00:00', '01:00', '02:00',
-#             '03:00', '04:00', '05:00',
-#             '06:00', '07:00', '08:00',
-#             '09:00', '10:00', '11:00',
-#             '12:00', '13:00', '14:00',
-#             '15:00', '16:00', '17:00',
-#             '18:00', '19:00', '20:00',
-#             '21:00', '22:00', '23:00',
-#         ],
-        'time': [
-            '00:00',
-            '03:00',
-            '06:00',
-            '09:00',
-            '12:00',
-            '15:00',
-            '18:00',
-            '21:00',
-        ],
-    },
-    OUTVAR)
+dataset = "reanalysis-era5-single-levels"
+request = {
+    "product_type": ["reanalysis"],
+    "variable": [
+        "10m_u_component_of_wind",
+        "10m_v_component_of_wind",
+        "2m_dewpoint_temperature",
+        "2m_temperature",
+        "mean_total_precipitation_rate",
+        "surface_pressure",
+        "surface_solar_radiation_downwards",
+        "surface_thermal_radiation_downwards"
+    ],
+    "year": [OUTYEAR],
+    "month": [OUTMONTH],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": [
+            "00:00",
+            "03:00",
+            "06:00",
+            "09:00",
+            "12:00",
+            "15:00",
+            "18:00",
+            "21:00"
+    ],
+    "data_format": "netcdf",
+    "download_format": "unarchived"
+}
+target = OUTVAR
+
+client = cdsapi.Client()
+client.retrieve(dataset, request, target).download()
