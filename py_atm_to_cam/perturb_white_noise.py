6efc583e6b8be4268fdaf9cdf96e98e4693bccdc
Colin Zarzycki <zarzycki@ucar.edu>
2025-11-06 09:34:41 -0700

    generalize perturb_white_noise some more

--- py_atm_to_cam/perturb_white_noise.py
+++ py_atm_to_cam/perturb_white_noise.py
@@ -10,7 +10,7 @@ if len(sys.argv) != 2:
 
 basFileName = sys.argv[1]
 
-pertMag = 0.001
+pertMag = 0.00001
 print(f"We are perturbing: {basFileName}")
 
 pertMagPerc = pertMag * 100
@@ -20,12 +20,27 @@ print(f"We are using perturbation magnitude of: {pertMagPerc}%")
 # Open with 'r+' which allows to read + overwrite vars (but not file)
 basFile = Dataset(basFileName, 'r+')
 
-################## Q
-q = basFile.variables['Q'][:]
+################## Q var
+# Figure out what Q var we want to perturb
+potential_q_vars = ['Q','dpQ']
+varq = None
+
+for var in potential_q_vars:
+    if var in basFile.variables:
+        varq = var
+        break
+
+if varq is None:
+    print("No Q var found!")
+    basFile.close()
+    sys.exit(1)
+
+print(f"Using this Q var: {varq}")
+q = basFile.variables[varq][:]
 
 ################## 2D
-# Figure out what 2D var we want to perturb
-potential_2d_vars = ['PS', 'PS_dry']
+# Figure out what surface var we want to perturb
+potential_2d_vars = ['PS', 'PS_dry', 'PSDRY']
 var2d = None
 
 for var in potential_2d_vars:
@@ -43,7 +58,7 @@ ps = basFile.variables[var2d][:]
 
 ################## 3D
 # Figure out what 3D var we want to perturb
-potential_3d_vars = ['T', 'U', 'V', 'THETA']
+potential_3d_vars = ['T', 'THETA', 'U', 'V']
 var3d = None
 
 for var in potential_3d_vars:
@@ -68,15 +83,15 @@ np.random.seed(int(datetime.datetime.now().timestamp()))
 low = 1.0 - pertMag
 high = 1.0 + pertMag
 
-# Perturb the 3D var
-print(f"orig {var3d} max: {np.max(t)}")
-t = t * np.random.uniform(low, high, t.shape)
-print(f"new {var3d} max: {np.max(t)}")
-
 # Perturb Q
 print(f"orig Q max: {np.max(q)}")
 q = q * np.random.uniform(low, high, q.shape)
 print(f"new Q max: {np.max(q)}")
+
+# Perturb the 3D var
+print(f"orig {var3d} max: {np.max(t)}")
+t = t * np.random.uniform(low, high, t.shape)
+print(f"new {var3d} max: {np.max(t)}")
 
 # Perturb PS
 print(f"orig PS max: {np.max(ps)}")
@@ -84,9 +99,9 @@ ps = ps * np.random.uniform(low, high, ps.shape)
 print(f"new PS max: {np.max(ps)}")
 
 # Write perturbed variables back to basFile
+basFile.variables[varq][:] = q
 basFile.variables[var2d][:] = ps
 basFile.variables[var3d][:] = t
-basFile.variables['Q'][:] = q
 
 basFile.close()
 
